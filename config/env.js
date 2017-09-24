bf38ef78d9818df1d59bf6eb911422ca09abc7ec
Florent Roques <roques.florent@gmail.com>
2017-09-25 00:02:14 +0200

    added IS_IOS const

--- config/env.js
+++ config/env.js
@@ -5,5 +5,7 @@ import { Platform } from 'react-native';
 export default {
   IS_ENV_DEVELOPMENT: __DEV__ || false,
   IS_ANDROID: Platform.OS === 'android',
-  IS_MATERIAL_DESIGN_SUPPORTED: Platform.OS === 'android' && Platform.Version >= 21,
+  IS_IOS: Platform.OS === 'ios',
+  IS_MATERIAL_DESIGN_SUPPORTED:
+    Platform.OS === 'android' && Platform.Version >= 21,
 };
