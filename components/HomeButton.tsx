adb2a7ac6854a39412bbd918d0a81c7a2bbb77d3
florentroques <florentroques@users.noreply.github.com>
2021-11-21 15:24:38 +0100

    clean unused code

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Button, Text, View, TouchableOpacity } from 'react-native';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import { View, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import { ActionCreators as UndoActionCreators } from 'redux-undo';
 import { CommonActions, useNavigation } from '@react-navigation/native';
 
-import { backHome } from '../actions';
-
-const ICON_COLOR = '#014DA2';
 const CENTER_ICON_SIZE = 36;
-const BOTTOM_BAR_ICON_SIZE = 30;
 
 const HomeIcon = ({ tintColor }: { tintColor: string }) => (
   <MaterialIcons
@@ -47,7 +40,6 @@ const HomeButton = ({
       }}>
       <TouchableOpacity
         onPress={() => {
-          onHomePress();
           navigation.dispatch(resetAction);
         }}
         style={{
@@ -59,19 +51,4 @@ const HomeButton = ({
   );
 };
 
-// TODO specify any
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    // onReset: () => dispatch(UndoActionCreators.clearHistory()),
-    onHomePress: () => {
-      dispatch(UndoActionCreators.clearHistory());
-      dispatch(backHome());
-    },
-    // onUndo: () => dispatch(UndoActionCreators.undo()),
-    onRedo: () => dispatch(UndoActionCreators.redo()),
-  };
-};
-
-const enhance = compose(connect(null, mapDispatchToProps));
-
-export default enhance(HomeButton);
+export default HomeButton;
