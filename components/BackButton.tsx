77aff662fadd6afcc01200e4cea9904b26228797
florentroques <florentroques@users.noreply.github.com>
2021-11-21 15:20:57 +0100

    remove unused code

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -3,16 +3,9 @@ import { View } from 'react-native';
 import { HeaderBackButton } from '@react-navigation/elements';
 import { useNavigation } from '@react-navigation/native';
 
-import { compose } from 'redux';
-import { connect } from 'react-redux';
 import { MaterialIcons } from '@expo/vector-icons';
-import { ActionCreators as UndoActionCreators } from 'redux-undo';
 
-import { goBack } from '../actions';
-
-const ICON_COLOR = '#014DA2';
 const CENTER_ICON_SIZE = 36;
-const BOTTOM_BAR_ICON_SIZE = 30;
 
 const HomeIcon = ({ tintColor }: { tintColor: string }) => (
   <MaterialIcons
@@ -39,7 +32,6 @@ const BackButton = ({ tintColor }: { tintColor: string }) => {
       <HeaderBackButton
         onPress={() => {
           navigation.goBack();
-          // onBackPress();
         }}
         tintColor={tintColor}
       />
@@ -47,19 +39,4 @@ const BackButton = ({ tintColor }: { tintColor: string }) => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onBackPress: () => {
-      // dispatch(UndoActionCreators.undo());
-      // dispatch(goBack());
-    },
-    // onUndo: () => dispatch(UndoActionCreators.undo()),
-    onRedo: () => {
-      // dispatch(UndoActionCreators.redo())
-    },
-  };
-};
-
-const enhance = compose(connect(null, mapDispatchToProps));
-
-export default enhance(BackButton);
+export default BackButton;
